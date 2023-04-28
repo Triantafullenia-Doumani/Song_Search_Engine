0730487270cffab67c6399f08720c534a1643f48
Maria Tsigkou <maria,tsigkou@yahoo.com>
2023-04-28 18:51:25 +0300

    save searching history

--- src/main/java/org/LuceneGUI.java
+++ src/main/java/org/LuceneGUI.java
@@ -6,6 +6,8 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.util.List;
 
@@ -15,10 +17,15 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionEvent;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
 import javax.swing.text.Highlighter;
@@ -30,7 +37,7 @@ import org.apache.lucene.queryparser.classic.ParseException;
 
 import com.opencsv.exceptions.CsvException;
 
-public class LuceneGUI implements ActionListener {
+public class LuceneGUI implements ActionListener,DocumentListener,MouseListener {
 
     // Declare instance variables for the GUI components
 	private JFrame frame;
@@ -42,6 +49,7 @@ public class LuceneGUI implements ActionListener {
 	private JButton prevButton;
 	private JButton nextButton;
 	private JCheckBox groupingCheckBox;
+	private JList<String> similarityList;
 	
 	private int currentPage = 1;
     private Searcher searcher;
@@ -62,25 +70,32 @@ public class LuceneGUI implements ActionListener {
 		contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		contentPane.setLayout(new BorderLayout());
 		
+
 		
 		// Create the query panel with the query text field, field combo box, search button and grouping checkbox
 		JPanel queryPanel = new JPanel(new GridLayout(1, 0, 10, 0));
 		queryTextField = new JTextField();
 		fieldComboBox = new JComboBox<>(new String[]{"General Search","Artist", "Title", "Album", "Date", "Lyrics", "Year"});
 		searchButton = new JButton("Search");
-		groupingCheckBox = new JCheckBox("Sort results by Artist", true);
-
+		groupingCheckBox = new JCheckBox("Sort results by Artist", false);
 		searchButton.addActionListener(this);
+		queryTextField.getDocument().addDocumentListener(this);
 		queryPanel.add(queryTextField);
 		queryPanel.add(fieldComboBox);
 		queryPanel.add(searchButton);
 		queryPanel.add(groupingCheckBox);
-
+		
 		// Create the results panel with the results text area and pagination controls
 		JPanel resultsPanel = new JPanel(new BorderLayout());
 		resultsTextArea = new JTextArea(20, 40);
 		resultsTextArea.setEditable(false);
-		JScrollPane scrollPane = new JScrollPane(resultsTextArea);
+		JScrollPane scrollPaneResults = new JScrollPane(resultsTextArea);
+		similarityList = new JList<>(new String[] {});
+		similarityList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);		
+		similarityList.setVisibleRowCount(-1);
+		JScrollPane scrollPaneSimilarity = new JScrollPane(similarityList);
+		similarityList.addMouseListener(this);
+		
 		pageNumberLabel = new JLabel("Page " + currentPage);
 		prevButton = new JButton("Previous");
 		prevButton.addActionListener(this);
@@ -92,9 +107,11 @@ public class LuceneGUI implements ActionListener {
 		paginationPanel.add(prevButton);
 		paginationPanel.add(pageNumberLabel);
 		paginationPanel.add(nextButton);
-		resultsPanel.add(scrollPane, BorderLayout.CENTER);
+		resultsPanel.add(scrollPaneResults, BorderLayout.CENTER);
 		resultsPanel.add(paginationPanel, BorderLayout.SOUTH);
-
+		resultsPanel.add(scrollPaneSimilarity,BorderLayout.WEST);
+		
+		
 		// Add the query panel and results panel to the content pane
 		contentPane.add(queryPanel, BorderLayout.NORTH);
 		contentPane.add(resultsPanel, BorderLayout.CENTER);
@@ -110,6 +127,7 @@ public class LuceneGUI implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent event) {
     	boolean isGrouped;
+    	
         if (event.getActionCommand().equals("Search")) {
         	String queryString = queryTextField.getText().trim();
         	String field = (String) fieldComboBox.getSelectedItem();
@@ -121,12 +139,14 @@ public class LuceneGUI implements ActionListener {
             List<Document> results;
             try {
                 results = searcher.search(queryString, field,isGrouped);
+                //similarityList.addEventListener("click",valueChanged);
                 displayResults(results);
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (ParseException e) {
                 e.printStackTrace();
             }
+            
         } else if (event.getActionCommand().equals("Next")) {
             try {
                 if (searcher.hasNextPage()) {
@@ -155,20 +175,24 @@ public class LuceneGUI implements ActionListener {
         
     }
     private void displayResults(List<Document> results) {
+    	boolean isGrouped;
     	String queryString = queryTextField.getText().trim();
     	String field = (String) fieldComboBox.getSelectedItem();
         pageNumberLabel.setText("Page " + currentPage);
         resultsTextArea.setText("");
         Highlighter highlighter = resultsTextArea.getHighlighter();
-        highlighter.removeAllHighlights();
+        highlighter.removeAllHighlights();        
         if(groupingCheckBox.isSelected()) {
-        	
-        }
+        	isGrouped = true;
+    	} else {
+    		isGrouped = false;
+    	}
         for (Document result : results) {
             String title = result.get("Title");
             String artist = result.get("Artist");
             String year = result.get("Year");
             String resultString = title + " by " + artist + " (" + year + ")\n";
+
             resultsTextArea.append(resultString);
             int startIndex = resultString.indexOf(queryString);
             if (startIndex != -1) {
@@ -184,6 +208,7 @@ public class LuceneGUI implements ActionListener {
             }
             
         }
+        
         // Enable or disable the "Previous" button based on the value of searcher.hasNextPage()
         if (searcher.hasNextPage()) {
             prevButton.setEnabled(true);
@@ -198,10 +223,42 @@ public class LuceneGUI implements ActionListener {
             nextButton.setEnabled(false);
         }
     }
+    
+    
+    //Document Listener methods
+    public void changedUpdate(DocumentEvent event) {
+        updateSuggestions();
+    }
+    public void removeUpdate(DocumentEvent event) {
+        updateSuggestions();
+    }
+    public void insertUpdate(DocumentEvent event) {
+        updateSuggestions();
+    }
+    public void updateSuggestions() {
+        String input = queryTextField.getText();
+        List<String> historyList = searcher.searchHistory(input);
+        similarityList.setListData(historyList.toArray(new String[] {}));
+    }
+    
+    //Mouse Listener methods
+    public void mousePressed(MouseEvent event) {}
+    
+    public void mouseReleased(MouseEvent event) {}
 
+    public void mouseEntered(MouseEvent event) {}
 
-
-
+    public void mouseExited(MouseEvent event) {}
+ 
+    public void mouseClicked(MouseEvent event) {
+    	if (event.getClickCount() == 2) {
+            int index = similarityList.locationToIndex(event.getPoint());
+            Object suggestedQuery = similarityList.getModel().getElementAt(index);
+            String suggestedQueryText = suggestedQuery.toString();
+            queryTextField.setText(suggestedQueryText);
+        }
+    }
+    
     public static void main(String[] args) throws IOException, CsvException {
         new StandardIndexer();
         new KeywordIndexer();
