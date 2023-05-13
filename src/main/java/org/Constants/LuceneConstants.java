9c36ab6e62eb72a8745268394ced0e5efb31ccf1
Triantafullenia-Doumani <triantafyllenia@tuta.io>
2023-05-14 01:35:20 +0300

    fix standard indexer

--- src/main/java/org/Constants/LuceneConstants.java
+++ src/main/java/org/Constants/LuceneConstants.java
@@ -5,6 +5,7 @@ public class LuceneConstants {
    public static final String CSV_PATH_AND_FILE_NAME = "data/final_dataset.csv";
    public static final String STANDARD_INDEX_FILE_PATH = "data/index/standard";
    public static final String KEYWORD_INDEX_FILE_PATH = "data/index/keyword";
+   public static final String MODEL_PATH = "data/model/word2vec.model";
    public static final int PAGE_SIZE = 10;
    public static final String GROUP = "Year";
 }
--- src/main/java/org/Indexer/StandardIndexer.java
+++ src/main/java/org/Indexer/StandardIndexer.java
@@ -25,77 +25,74 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
 
-
 public class StandardIndexer {
 
 	IndexWriter indexWriter;
 	CSVParser parser;
 
-	public StandardIndexer() throws IOException {
-		
-		System.out.println("Standard Analyzer: ");
-	    // Initialize index directory
-		Directory index = FSDirectory.open(Paths.get(LuceneConstants.STANDARD_INDEX_FILE_PATH));
-	    if (!DirectoryReader.indexExists(index)) {
-	    	System.out.println("	Index does not exist");
-	    } else {
-            IndexReader reader = DirectoryReader.open(index);
-            System.out.println("	Number of documents in existing Index: " + reader.numDocs());
-            reader.close();
-        }
-		// Create index writer configuration
-		IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
-		
-		// Create index writer
-		this.indexWriter = new IndexWriter(index, config);
-	    // Delete existing index, if it exists
-	    if (DirectoryReader.indexExists(index)) {
-	    	System.out.println("	Deleting existing index...");
-	    	this.indexWriter.deleteAll();
-	    }
-		// Parse CSV file
-		CSVFormat format = CSVFormat.DEFAULT.withFirstRecordAsHeader();
-		
-		this.parser = CSVParser.parse(new File(LuceneConstants.CSV_PATH_AND_FILE_NAME), StandardCharsets.UTF_8, format);
-		
-		// Check if header is present in CSV file
-		Map<String, Integer> headerMap = parser.getHeaderMap();
-		if (headerMap == null) {
-			indexWriter.close();
-		    throw new IOException("	CSV file does not have header");
-		}
-		// Iterate over records and add to index
-		for (CSVRecord record : parser) {
-		    // Create new document for each record
-		    Document doc = new Document();
-		    
-		    // Add each field to the document
-		    for (String header : headerMap.keySet()) {
-		        // Get the text value for the header
-		        String text = record.get(header);
-		        
-		        // @TOFIX synexisei na bazei ta anepithimita string mesa
-		        if((text.equals("lyrics for this song have yet to be released please check back once the song has been released")) || (text.equals("unreleased songs")) || (text.equals("unreleased"))) {
-		        	text = "";	
-		        }else {
-		        	// Preprocess text
-		            text = preprocessText(text);
-		        }
-				if (header.equals(LuceneConstants.GROUP)) {
-					doc.add(new SortedDocValuesField (header, new BytesRef(text) ));
-					doc.add(new StoredField(header, text));
-				}else {
-					doc.add(new TextField(header, text, Field.Store.YES));
-				}
-		    }
-		    // Add document to the index
-		    this.indexWriter.addDocument(doc);
-		  }
-		 System.out.println("	New Index created successfully: Number of documents in the new index: " + this.indexWriter.numRamDocs()+"\n");
 
-		 close();
+	public StandardIndexer() throws IOException {
+
+		    System.out.println("Standard Analyzer: ");
+		    // Initialize index directory
+		    Directory index = FSDirectory.open(Paths.get(LuceneConstants.STANDARD_INDEX_FILE_PATH));
+		    if (!DirectoryReader.indexExists(index)) {
+		        System.out.println("    Index does not exist");
+		    } else {
+		        IndexReader reader = DirectoryReader.open(index);
+		        System.out.println("    Number of documents in existing Index: " + reader.numDocs());
+		        reader.close();
+		    }
+		    // Create index writer configuration
+		    IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+
+		    // Create index writer
+		    this.indexWriter = new IndexWriter(index, config);
+		    // Delete existing index, if it exists
+		    if (DirectoryReader.indexExists(index)) {
+		        System.out.println("    Deleting existing index...");
+		        this.indexWriter.deleteAll();
+		    }
+		    // Parse CSV file
+		    CSVFormat format = CSVFormat.DEFAULT.withFirstRecordAsHeader();
+
+		    this.parser = CSVParser.parse(new File(LuceneConstants.CSV_PATH_AND_FILE_NAME), StandardCharsets.UTF_8, format);
+
+			// Check if header is present in CSV file
+			Map<String, Integer> headerMap = parser.getHeaderMap();
+			if (headerMap == null) {
+				indexWriter.close();
+			    throw new IOException("CSV file does not have header");
+			}
+			// Iterate over records and add to index
+			for (CSVRecord record : parser) {
+			    // Create new document for each record
+			    Document doc = new Document();
+			    
+			    // Add each field to the document
+			    for (String header : headerMap.keySet()) {
+			        // Get the text value for the header
+			        String text = record.get(header);
+		            if((text.equals("lyrics for this song have yet to be released please check back once the song has been released")) || (text.equals("unreleased songs")) || (text.equals("unreleased"))) {
+		            	text = "";	
+		            }else {
+		            	// Preprocess text
+			            text = preprocessText(text);
+		            }	        
+					if (header.equals(LuceneConstants.GROUP)) {
+						doc.add(new SortedDocValuesField (header, new BytesRef(text) ));
+						doc.add(new StoredField(header, text));
+					}else {
+						doc.add(new TextField(header, text, Field.Store.YES));
+					}
+			    }
+			    // Add document to the index
+			    this.indexWriter.addDocument(doc);
+			  }
+			 System.out.println("	New Index created successfully: Number of documents in the new index: " + this.indexWriter.numRamDocs()+"\n");
+			 close();
 	}
-   
+
    private static String preprocessText(String text) {
 		// Remove all punctuation marks except hyphens, periods, and digits
 		text = text.replaceAll("[^a-zA-Z0-9\\s.-]", "");
